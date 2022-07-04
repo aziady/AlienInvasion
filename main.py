999c589504dfb5091dc91e04b39e6eeb9e76c055
Abe Ziady <abraham.ziady@gmail.com>
2022-07-04 14:43:10 -0400

    Second Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# In Alien Invasion, the player controls a ship that appears at the bottom center of the screen.
+# The player can move the ship right and left using the arrow keys and shoot bullets using the
+# spacebar. When the game begins, a fleet of aliens fills the sky and moves across and down
+# the screen. The player shoots and destroys the aliens. If the player shoots al the aliens,
+# a new fleet appears that moves faster than the previous fleet. If any alien hits the player's
+# ship or reaches the bottom of the screen, the player loses a ship. If the player loses three
+# ships, the game is over.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
