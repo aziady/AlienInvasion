d541559b09fdcb0cda3d8bb7bc57aadbd54c3bd2
Abe Ziady <abraham.ziady@gmail.com>
2022-07-05 16:28:45 -0400

    Starting the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -17,6 +18,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
 
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
@@ -37,13 +41,13 @@ def run_game():
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
 
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
